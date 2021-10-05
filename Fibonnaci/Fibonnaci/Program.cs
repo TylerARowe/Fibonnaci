6c0086f0e3bffa5ef39dd4b734e7de5f059155b2
TylerARowe <tyl3rr0we95@gmail.com>
2021-10-05 17:03:07 -0500

    Commiting to the second commit.

--- Fibonnaci/Fibonnaci/Program.cs
+++ Fibonnaci/Fibonnaci/Program.cs
@@ -11,13 +11,24 @@ namespace Fibonnaci
         public static void Main(string[] args)
         {
             int n1 = 0, n2 = 1, n3, i;
-            for (i = 0; i < 42; ++i)     
+            int evenVariable = 0;
+            for (i = 0; i < 41; ++i)     
             {
                 n3 = n1 + n2;
                 Console.Write(n3 + " ");
                 n1 = n2;
                 n2 = n3;
+
+                
+                if (n3 % 2 == 0)
+                {
+                    evenVariable += n3;
+                }
+
+               
             }
+
+            Console.WriteLine($"\n\n {evenVariable}");
         }
     }
 }
